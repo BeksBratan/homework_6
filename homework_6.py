0d2c29300a1c07ebc1d59ff4cc5d33b89348d319
BeksBratan <93837624+BeksBratan@users.noreply.github.com>
2021-11-16 23:52:01 +0600

    Add files via upload

--- homework_6.py
+++ homework_6.py
@@ -1,1 +1,11 @@
-homework_6.py
+class Hm:
+    def meaning(self):
+        numbers = [2, 5, 7, 9, 12, 35, 15, 67]
+        my_number = 40
+        for first in numbers:
+            for second in numbers:
+                if first+second == my_number:
+                    print([first, second])
+
+h = Hm()
+h.meaning()
