de85a307e09a9f03dddf2e96ea487693c611388e
BeksBratan <93837624+BeksBratan@users.noreply.github.com>
2021-11-18 21:15:45 +0600

    Update homework_6.py

--- homework_6.py
+++ homework_6.py
@@ -1,11 +1,17 @@
-class Hm:
-    def meaning(self):
-        numbers = [2, 5, 7, 9, 12, 35, 15, 67]
-        my_number = 40
-        for first in numbers:
-            for second in numbers:
-                if first+second == my_number:
-                    print([first, second])
-
-h = Hm()
+class Hm: 
+    def __init__(self): 
+        self.x = 0
+        self.y = 0
+ 
+    def meaning(self): 
+        numbers = [2, 5, 7, 9, 12, 35, 15, 67] 
+ 
+        my_number = 40 
+        for self.x in numbers: 
+            for self.y in range(numbers.index(self.x) + 1, len(numbers)): 
+                if self.x + numbers[self.y] == my_number: 
+                    print([numbers.index(self.x), self.y]) 
+ 
+ 
+h = Hm() 
 h.meaning()
